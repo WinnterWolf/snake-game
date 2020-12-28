ca968e2b264073093aac7008bedd1886b4874838
WinnterWolf <patrick.winter@outlook.com.br>
2020-12-28 18:08:24 -0400

    fazendo a skane crescer e adicionando aleatoriedade no spawn da comida

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ snake[0] = {
 let direction = "right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
-    Y: Math.floor(Math.random() * 15 + 1) * box
+    y: Math.floor(Math.random() * 15 + 1) * box
     
 }
 
@@ -42,9 +42,9 @@ function update (event){
 
 function iniciarJogo(){
 
-    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x > 16*box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y > 16*box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16*box;
 
     criarBG();
@@ -59,7 +59,13 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
 
     let newHead = {
         x: snakeX, 
